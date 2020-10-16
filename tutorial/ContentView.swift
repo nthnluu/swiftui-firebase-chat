d97b2e84f8758f7ceec34e57cc19abb9cc5a881b
nathanluu <nate1299@me.com>
2020-10-16 11:26:35 -0700

    Initial Commit

---
+++ tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorial
+//
+//  Created by Nathan Luu on 10/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tutorial/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by Nathan Luu on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
